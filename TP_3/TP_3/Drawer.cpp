a3ffc63ee2257c44bba4021c9d9f89c4bfb5335d
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2018-08-16 22:59:55 -0300

    drawer se empieza a formar

--- TP_3/TP_3/Drawer.cpp
+++ TP_3/TP_3/Drawer.cpp
@@ -2,11 +2,27 @@
 
 
 
-Drawer::Drawer()
+Drawer::Drawer(uint w, uint h)
 {
+	Hcount = h;
+	Wcount = w;
+	al_init();
+	al_init_image_addon();
+	al_init_font_addon();
 }
 
-
-Drawer::~Drawer()
+void Drawer::
+allegro_init()
 {
+	display = al_create_display(DISP_WIDTH, DISP_HEIGHT);	//se crea el display de un tamaño determinado. El tamaño de las baldosas depende del tamaño del display
+	robotImage = al_load_bitmap("images/robot.png");
+	
+}
+
+void Drawer::
+allegro_destroy()
+{
+	al_shutdown_image_addon();
+	al_shutdown_font_addon();
+	al_destroy_display(display);
 }
--- TP_3/TP_3/Drawer.h
+++ TP_3/TP_3/Drawer.h
@@ -2,16 +2,34 @@
 #ifndef _DRAWER_
 #define _DRAWER_
 
+#define DISP_WIDTH 800
+#define DISP_HEIGHT 800
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
 #include "Common_header.h"
+#include "Floor.h"
+#include "Robot.h"
 
 class Drawer
 {
+	ALLEGRO_DISPLAY * display;
+	ALLEGRO_BITMAP * robotImage;
+	uint Hcount;
+	uint Wcount;
+	errNo_t err;
+	Robot * robots;
+	Floor * floor;
 
 public:
-	Drawer();
-	~Drawer();
+	Drawer(uint w, uint h);
+	
+	void allegro_init();
+	void allegro_destroy();
+	void draw_tile();				//pintar solo las baldosas
+	void draw_robot();				//pintar solo los robots
+	void draw_display();			//metodo que, utilizando las funciones de arriba, pinta todo el display
 };
 
 #endif

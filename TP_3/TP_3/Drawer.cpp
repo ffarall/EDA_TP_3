9e78ca60be62d581fa7766123aa754c656f71c56
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2018-08-19 20:54:13 -0300

    detalles

--- TP_3/TP_3/Drawer.cpp
+++ TP_3/TP_3/Drawer.cpp
@@ -1,12 +1,15 @@
 #include "Drawer.h"
 
-Drawer::Drawer(uint w, uint h)	//constructor
+Drawer::Drawer(uint w, uint h, Robot * robotP, uint rc,Floor * floorP)	//constructor
 {
 	Hcount = h;	//cantidad de baldosas en y
 	Wcount = w;	//cantidad de baldosas en x
 	dispHeight = Hcount * SINGLE_TILE;	//altura del display
 	dispWidth = Wcount * SINGLE_TILE;	//ancho del display
-
+	robots = robotP;
+	robotCount = rc;
+	floor = floorP;
+	font = al_create_builtin_font();
 }
 
 void Drawer::
@@ -51,14 +54,17 @@ allegro_init()	//inicializa todas las funciones de allegro
 void Drawer::
 allegro_destroy()	//cierra todas las funciones de allegro
 {
-	al_shutdown_image_addon();
-	al_shutdown_font_addon();
-	al_shutdown_primitives_addon();
 	al_destroy_bitmap(dirtImage);
 	al_destroy_bitmap(robotImage);
 	al_destroy_display(display);
+	al_destroy_font(font);
+	
+	al_shutdown_image_addon();
+	al_shutdown_font_addon();
+	al_shutdown_primitives_addon();
 }
 
+//Metodos de modo 1
 void Drawer::
 draw_init_display()	//dibuja las baldosas y las inicializa en sucio
 {
@@ -82,32 +88,32 @@ draw_init_display()	//dibuja las baldosas y las inicializa en sucio
 	}
 	al_flip_display();
 }
-/*
+
 void Drawer::
-update_display(Floor * floor, Robot * robots)	//hace el update del display a medida que se van moviendo los robots
+update_display()	//hace el update del display a medida que se van moviendo los robots
 {
 	al_clear_to_color(al_map_rgb(255, 255, 255));
 
-	for (int i = 0; i < dispWidth; i += SINGLE_TILE)
+	for (uint i = 0; i < dispWidth; i += SINGLE_TILE)
 	{
 		al_draw_line(i, 0, i, dispHeight, al_map_rgb(0, 0, 0), 1.0);
 	}
 
-	for (int i = 0; i < dispHeight; i += SINGLE_TILE)
+	for (uint i = 0; i < dispHeight; i += SINGLE_TILE)
 	{
 		al_draw_line(0, i, dispWidth, i, al_map_rgb(0, 0, 0), 1.0);
 	}
-	for (int i = 0; i < dispWidth; i += SINGLE_TILE)
+	for (uint i = 0; i < dispWidth; i += SINGLE_TILE)
 	{
-		for (int j = 0; j < dispHeight; j += SINGLE_TILE)
+		for (uint j = 0; j < dispHeight; j += SINGLE_TILE)
 		{
-			for(int k = 0; k < rc; k++)
+			for(uint k = 0; k < robotCount; k++)
 			{
 				if(floor->is_dirty(i,j))		//si la baldosa en (i,j) esta sucia, pintar dirt
 				{
 					al_draw_scaled_bitmap(dirtImage, 0, 0, al_get_bitmap_width(dirtImage), al_get_bitmap_height(dirtImage), i, j, SINGLE_TILE, SINGLE_TILE, 0);
 				}
-				if(Robot[k]->get_pos().x == i && Robot[k]->get_pos().x == j)			//si hay un robot en (i,j), pintar el robot
+				if(robots[k]->get_robotPos().x == i && robots[k]->get_robotPos().y == j)			//si hay un robot en (i,j), pintar el robot
 				{
 					al_draw_scaled_bitmap(robotImage, 0, 0, al_get_bitmap_width(robotImage), al_get_bitmap_height(robotImage), i, j, SINGLE_TILE, SINGLE_TILE, 0);
 				}
@@ -115,6 +121,16 @@ update_display(Floor * floor, Robot * robots)	//hace el update del display a med
 		}
 	}
 	al_flip_display();
-}*/
+}
 
+void Drawer::
+show_ticks(uint ticks)	//muestra en pantalla los ticks utilizados
+{
+	al_draw_filled_rectangle(0.25*dispHeight, 0.25*dispWidth,0.75*dispHeight,0.75*dispWidth,al_map_rgb(239,246,41));
+	char * text;
+	sprintf(text, "Numero de ticks: %d", ticks);
+	al_draw_text(font, al_map_rgb(0, 0, 0), dispWidth / 2.0, dispHeight / 2.0, ALLEGRO_ALIGN_CENTER, text);
+}
+
+//metodos de modo 2
 
--- TP_3/TP_3/Drawer.h
+++ TP_3/TP_3/Drawer.h
@@ -18,21 +18,29 @@ class Drawer
 	ALLEGRO_DISPLAY * display;
 	ALLEGRO_BITMAP * robotImage;
 	ALLEGRO_BITMAP * dirtImage;
-	int dispWidth;
-	int dispHeight;
+	ALLEGRO_FONT * font;
+	uint dispWidth;
+	uint dispHeight;
 	uint Hcount;
 	uint Wcount;
+	uint robotCount;
 	errorType_t err;
 	Robot * robots;
 	Floor * floor;
 
 public:
-	Drawer(uint w, uint h);
+	Drawer(uint w, uint h, Robot * robotP, uint rc,Floor * floorP);
 	
 	void allegro_init();
 	void allegro_destroy();
+
+	//modo 1
 	void draw_init_display();
 	void update_display();
+	void show_ticks(uint ticks);
+
+	//modo 2
+
 };
 
 #endif
